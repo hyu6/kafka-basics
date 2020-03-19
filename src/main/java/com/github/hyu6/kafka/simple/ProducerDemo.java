b96b3f8850c23e8e6d4b9c7b10c0e5afebddfe83
Haoran Yu <haoranyu6@gmail.com>
2020-03-19 01:52:59 -0700

    create the project

---
+++ src/main/java/com/github/hyu6/kafka/simple/ProducerDemo.java
@@ -0,0 +1,16 @@
+package com.github.hyu6.kafka.simple;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+
+        // create producer properties
+        Properties properties = new Properties();
+
+        // create the producer
+
+        // send data
+
+    }
+}
